9c3b995a25a8994bb2cf9591387b67ed0f76393b
kefir <kefir@832c1c66-4827-0410-8465-ccb17913cd09>
2010-09-11 12:36:07 +0000

    Mapping attachment content to MySQL longblob.
    git-svn-id: http://svn.codehaus.org/grails-plugins/grails-asynchronous-mail/trunk@64416 832c1c66-4827-0410-8465-ccb17913cd09

--- grails-app/domain/ru/perm/kefir/asynchronousmail/AsynchronousMailAttachment.groovy
+++ grails-app/domain/ru/perm/kefir/asynchronousmail/AsynchronousMailAttachment.groovy
@@ -2,6 +2,7 @@ package ru.perm.kefir.asynchronousmail
 
 class AsynchronousMailAttachment implements Serializable{
     public static final DEFAULT_MIME_TYPE = 'application/octet-stream';
+    private static final SIZE_30_MB = 30*1024*1024;
 
     String attachmentName;
     String mimeType = DEFAULT_MIME_TYPE;
@@ -12,6 +13,6 @@ class AsynchronousMailAttachment implements Serializable{
     static constraints = {
         attachmentName(nullable:false, blank:false);
         mimeType(nullable:false);
-        content(nullable:false);
+        content(nullable:false, maxSize:SIZE_30_MB);
     }
 }
