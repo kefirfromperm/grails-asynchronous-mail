488414ab914da33c0ad1549855354557db60f839
Burt Beckwith <beckwithb@vmware.com>
2012-11-30 01:36:34 -0500

    using a name for the targets so the scripts can be imported into other scripts and called; also changed use of deprecated Ant to ant

--- scripts/CreateAsynchronousMailController.groovy
+++ scripts/CreateAsynchronousMailController.groovy
@@ -1,32 +1,34 @@
-target('default': "Create controller for manage asynchronous messages") {
-    Ant.copy(
+target(createAsynchronousMailController: "Create controller for manage asynchronous messages") {
+    ant.copy(
             file: "${asynchronousMailPluginDir}/src/templates/controllers/AsynchronousMailController.groovy",
             tofile: "$basedir/grails-app/controllers/AsynchronousMailController.groovy",
             overwrite: false
-    );
-    Ant.copy(
+    )
+    ant.copy(
             file: "${asynchronousMailPluginDir}/src/templates/views/asynchronousMail/list.gsp",
             tofile: "$basedir/grails-app/views/asynchronousMail/list.gsp",
             overwrite: false
-    );
-    Ant.copy(
+    )
+    ant.copy(
             file: "${asynchronousMailPluginDir}/src/templates/views/asynchronousMail/show.gsp",
             tofile: "$basedir/grails-app/views/asynchronousMail/show.gsp",
             overwrite: false
-    );
-    Ant.copy(
+    )
+    ant.copy(
             file: "${asynchronousMailPluginDir}/src/templates/views/asynchronousMail/edit.gsp",
             tofile: "$basedir/grails-app/views/asynchronousMail/edit.gsp",
             overwrite: false
-    );
-    Ant.copy(
+    )
+    ant.copy(
             file: "${asynchronousMailPluginDir}/src/templates/views/asynchronousMail/_listAddr.gsp",
             tofile: "$basedir/grails-app/views/asynchronousMail/_listAddr.gsp",
             overwrite: false
-    );
-    Ant.copy(
+    )
+    ant.copy(
             file: "${asynchronousMailPluginDir}/src/templates/views/asynchronousMail/_flashMessage.gsp",
             tofile: "$basedir/grails-app/views/asynchronousMail/_flashMessage.gsp",
             overwrite: false
-    );
+    )
 }
+
+setDefaultTarget 'createAsynchronousMailController'
--- scripts/InstallAsynchronousMailConfig.groovy
+++ scripts/InstallAsynchronousMailConfig.groovy
@@ -2,10 +2,12 @@
  * Create config file for asynchronous mail
  */
 
-target('default': 'Create config file for asynchronous mail grails plugin') {
-    Ant.copy(
+target(installAsynchronousMailConfig: 'Create config file for asynchronous mail grails plugin') {
+    ant.copy(
             file: "${asynchronousMailPluginDir}/grails-app/conf/DefaultAsynchronousMailConfig.groovy",
             tofile: "$basedir/grails-app/conf/AsynchronousMailConfig.groovy",
             overwrite: false
-    );
+    )
 }
+
+setDefaultTarget 'installAsynchronousMailConfig'
