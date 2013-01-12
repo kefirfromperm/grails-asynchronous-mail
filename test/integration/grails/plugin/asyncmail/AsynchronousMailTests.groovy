71c274518ee241011ed7ef341c7b529007c09cba
Vitalii Samolovskikh <kefir@perm.ru>
2013-01-12 15:18:25 +0600

    Test for AsynchronousMailService.sendMail()

--- test/integration/grails/plugin/asyncmail/AsynchronousMailTests.groovy
+++ test/integration/grails/plugin/asyncmail/AsynchronousMailTests.groovy
@@ -6,11 +6,24 @@ package grails.plugin.asyncmail
  * @author Vitalii Samolovskikh aka Kefir
  */
 class AsynchronousMailTests extends GroovyTestCase {
+    public static final String VALUE_MAIL = 'test@example.com'
+
     AsynchronousMailService asynchronousMailService;
     AsynchronousMailService asyncMailService;
 
     void testSendAsynchronousMail(){
-        assertTrue(false)
+        asyncMailService.sendMail {
+            to VALUE_MAIL
+            subject 'Test'
+            text 'Test'
+
+            immediate false
+        }
+
+        AsynchronousMailMessage message = AsynchronousMailMessage.findAll()[0]
+
+        assertEquals(VALUE_MAIL, message.to[0])
+        assertEquals(MessageStatus.CREATED, message.status)
     }
 
     void testServiceAlias(){
