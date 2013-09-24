8656bf821c0009898de3606cfe2dfd663ece191c
Iván López <lopez.ivan@gmail.com>
2013-09-24 17:53:35 +0200

    Update quartz to version RC9

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,7 +16,7 @@ grails.project.dependency.resolution = {
         }
 
         compile ':mail:1.0.1'
-        compile ':quartz:1.0-RC7'
+        compile ':quartz:1.0-RC9'
 
         build(':release:2.1.0', ':rest-client-builder:1.0.3') {
             export = false
