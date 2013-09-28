e2b85bfcde0a7cb6687a4d1c00f2e355bcd5d344
Vitalii Samolovskikh <kefir@perm.ru>
2013-09-28 12:50:31 +0600

    Update dependency versions.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,12 +16,12 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime(":hibernate:$grailsVersion") {
+        runtime(":hibernate:2.0.0") {
             export = false
         }
 
         compile ':mail:1.0.1'
-        compile ':quartz:1.0-RC9'
+        compile ':quartz:1.0-RC10'
 
         build(':release:2.1.0', ':rest-client-builder:1.0.3') {
             export = false
