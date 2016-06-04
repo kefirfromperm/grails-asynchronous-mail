07684b364bee4e68b5a5004cc58da115727d469c
Vitalii Samolovskikh <kefirfromperm@gmail.com>
2016-06-04 12:17:04 +0500

    #56 delete attachments refactoring

--- grails-app/services/grails/plugin/asyncmail/AsynchronousMailPersistenceService.groovy
+++ grails-app/services/grails/plugin/asyncmail/AsynchronousMailPersistenceService.groovy
@@ -13,22 +13,12 @@ class AsynchronousMailPersistenceService implements GrailsConfigurationAware {
     }
 
     void delete(AsynchronousMailMessage message) {
-        //deleteAttachments(message)
         message.delete(flush:true)
     }
 
     void deleteAttachments(AsynchronousMailMessage message) {
-
-        def attachments = message.attachments
-
         message.attachments.clear()
-
         message.save(flush:true)
-
-        attachments.each{
-            it.delete()
-        }
-
     }
 
     AsynchronousMailMessage getMessage(long id){
